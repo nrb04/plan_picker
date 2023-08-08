778b1a08fd30f96f9b1feaadcc01f33e979f7bf9
nrb04 <nredoyan@gmail.com>
2023-08-08 21:55:28 +0600

    componands add home

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button } from "@material-tailwind/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       {" "}
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Button</Button>
+      <div className="flex flex-col ">
+        <progress
+          className="progress progress-secondary w-56"
+          value={0}
+          max="100"
+        ></progress>
+        <progress
+          className="progress progress-secondary w-56"
+          value="10"
+          max="100"
+        ></progress>
+        <progress
+          className="progress progress-secondary w-56"
+          value="40"
+          max="100"
+        ></progress>
+        <progress
+          className="progress progress-secondary w-56"
+          value="70"
+          max="100"
+        ></progress>
+        <progress
+          className="progress progress-secondary w-56"
+          value="100"
+          max="100"
+        ></progress>
+      </div>
+      <div className="text-red-800 text-6xl text-bold  "> hello </div>
     </>
   );
 }
