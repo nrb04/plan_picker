037038ffac942f5fb6cf6709b79c62dfcf95ac57
ptan01 <ptanjim01@gmail.com>
2023-08-14 12:11:38 +0600

    blog card add

--- src/LandingPage/Blog/Blog.jsx
+++ src/LandingPage/Blog/Blog.jsx
@@ -1,8 +1,22 @@
 import Lottie from "lottie-react";
 import bloganimition from "../../assets/contentCreation.json";
+import BlogCard from "./BlogCard";
+import { useEffect, useState } from "react";
 
 
 const Blog = () => {
+
+  const [topArticels, setTopArticels] = useState([])
+
+  useEffect(() => {
+    fetch("topArticels.json")
+      .then(res => res.json())
+      .then(data => setTopArticels(data))
+  }, [])
+
+
+
+
   return (
     <div className="">
       <div className="md:flex items-center my-10">
@@ -12,6 +26,11 @@ const Blog = () => {
         <div className="md:w-1/2">
           <Lottie animationData={bloganimition} loop={true} />
         </div>
+      </div>
+      <div className="grid md:gird-cols-2 lg:grid-cols-3">
+        {
+          topArticels.map(article => <BlogCard></BlogCard>)
+        }
       </div>
     </div>
   );
