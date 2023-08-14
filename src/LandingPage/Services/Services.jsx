5dff56b3e397f68b56f8486385c9a7412feb8537
nikkonbd <nikkon1998@gmail.com>
2023-08-14 19:04:31 +0600

    service added

--- src/LandingPage/Services/Services.jsx
+++ src/LandingPage/Services/Services.jsx
@@ -1,5 +1,95 @@
+import React from 'react';
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
+import { GiCheckMark } from 'react-icons/gi';
+import ServiceCard from './serviceCard/ServiceCard1';
+import ServiceCard2 from './serviceCard/serviceCard2';
+import ServiceCard3 from './serviceCard/ServiceCard3';
+import ServiceCard4 from './serviceCard/ServiceCard4';
+
 const Services = () => {
-  return <div>services page</div>;
+  return (
+    <div className="mx-4 md:mx-12 py-16 sm:px-10 lg:px-8">
+      <Card className="w-full flex-row">
+        {/* <CardHeader
+          shadow={false}
+          floated={false}
+          className="m-0 w-2/5 shrink-0 rounded-r-none"
+        >
+          <img
+            src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+            alt="card-image"
+            className="h-full w-full object-cover"
+          />
+        </CardHeader> */}
+        <CardBody>
+          <Typography variant="h6" color="gray" className="mb-4 uppercase">
+            startups
+          </Typography>
+          <Typography variant="h4" color="blue-gray" className="mb-2 text-4xl">
+          Pick the perfect plan for your team
+          </Typography>
+          <Typography color="gray" className="mb-3 font-normal">
+          Create organization-wide value with enterprise admin, security, and IT-oriented features to grow pipeline, close more deals, and hire faster. It was until recently a traditional boxed software company
+            selling licenses. Yet its own business model disruption is only part
+            of the story
+          </Typography>
+          <div className='mb-8'>
+            <div className='flex items-center gap-2'>
+            <GiCheckMark></GiCheckMark>
+            <p>Enable Single Sign-On and advanced user provisioning</p>
+            </div>
+            <div className='flex items-center gap-2'>
+            <GiCheckMark></GiCheckMark>
+            <p>Ensure compliance with an audit log and data deletion API</p>
+            </div>
+            <div className='flex items-center gap-2'>
+            <GiCheckMark></GiCheckMark>
+            <p>Claim your domain and work in one account</p>
+            </div>
+            <div className='flex items-center gap-2'>
+            <GiCheckMark></GiCheckMark>
+            <p>Use the full suite of Salesforce routing features</p>
+            </div>
+            <div className='flex items-center gap-2'>
+            <GiCheckMark></GiCheckMark>
+            <p>Access expedited support, onboarding and implementation</p>
+            </div>
+          </div>
+          <a href="#" className="inline-block">
+            <Button variant="text" className="flex items-center gap-2">
+              Learn More
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+                className="h-4 w-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                />
+              </svg>
+            </Button>
+          </a>
+        </CardBody>
+      </Card>
+      <div className='mt-16 md:flex gap-3'>
+        <ServiceCard></ServiceCard>
+        <ServiceCard2></ServiceCard2>
+        <ServiceCard3></ServiceCard3>
+        <ServiceCard4></ServiceCard4>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
---
+++ src/LandingPage/Services/serviceCard/ServiceCard2.jsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button,
+  } from "@material-tailwind/react";
+
+  function CheckIcon() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={2}
+        stroke="currentColor"
+        className="h-3 w-3"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M4.5 12.75l6 6 9-13.5"
+        />
+      </svg>
+    );
+  }
+
+const ServiceCard2 = () => {
+    return (
+        <div className='mb-6'>
+            <Card color="gray" variant="gradient" className="w-full md:h-[800px] md:max-w-[300px] p-8">
+                <CardHeader
+                    floated={false}
+                    shadow={false}
+                    color="transparent"
+                    className="m-0 mb-8 rounded-none border-b border-white/10 pb-8 text-center"
+                >
+                    <Typography
+                    variant="small"
+                    color="white"
+                    className="font-normal uppercase"
+                    >
+                    Essentials
+                    </Typography>
+                    <Typography
+                    variant="h1"
+                    color="white"
+                    className="mt-6 flex justify-center gap-1 text-7xl font-normal"
+                    >
+                    <span className="mt-2 text-4xl">$</span>08{" "}
+                    <span className="self-end text-4xl">/mo</span>
+                    </Typography>
+                </CardHeader>
+                <CardBody className="p-0">
+                    <ul className="flex flex-col gap-4">
+                    <li className="flex items-center gap-4">
+                        <span className="rounded-full border border-white/20 bg-white/20 p-1">
+                        <CheckIcon />
+                        </span>
+                        <Typography className="font-normal">Connect six calendars</Typography>
+                    </li>
+                    <li className="flex items-center gap-4">
+                        <span className="rounded-full border border-white/20 bg-white/20 p-1">
+                        <CheckIcon />
+                        </span>
+                        <Typography className="font-normal">Create unlimited event types</Typography>
+                    </li>
+                    <li className="flex items-center gap-4">
+                        <span className="rounded-full border border-white/20 bg-white/20 p-1">
+                        <CheckIcon />
+                        </span>
+                        <Typography className="font-normal">Create group events</Typography>
+                    </li>
+                    <li className="flex items-center gap-4">
+                        <span className="rounded-full border border-white/20 bg-white/20 p-1">
+                        <CheckIcon />
+                        </span>
+                        <Typography className="font-normal">Send your invitees email reminders and follow-ups</Typography>
+                    </li>
+                    <li className="flex items-center gap-4">
+                        <span className="rounded-full border border-white/20 bg-white/20 p-1">
+                        <CheckIcon />
+                        </span>
+                        <Typography className="font-normal">
+                        Life time technical support
+                        </Typography>
+                    </li>
+                    </ul>
+                </CardBody>
+                <CardFooter className="mt-12 p-0">
+                    <a href="#" className="inline-block md:mt-[184px]">
+                        <Button variant="text" className="flex hover:bg-gray-700 text-white items-center gap-2">
+                        Learn More
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth={2}
+                            className="h-4 w-4 text-white"
+                        >
+                            <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                            />
+                        </svg>
+                        </Button>
+                    </a>
+                </CardFooter>
+            </Card>
+        </div>
+    );
+};
+
+export default ServiceCard2;
