322f0c7e039611668d8e82c7e8bd269937b700fe
ptan01 <ptanjim01@gmail.com>
2023-08-16 16:26:16 +0600

    week sedule component create

--- src/allrouter/Router.jsx
+++ src/allrouter/Router.jsx
@@ -6,6 +6,7 @@ import ContactUS from "../LandingPage/contact/ContactUS";
 import About from "../LandingPage/aboutus/About";
 import Blog from "../LandingPage/Blog/Blog";
 import BlogDetails from "../LandingPage/Blog/blogDetails/BlogDetails";
+import WeeklySedule from "../components/WeeklySedule";
 
 const router = createBrowserRouter([
   {
@@ -31,6 +32,10 @@ const router = createBrowserRouter([
       {
         path: "/blog",
         element: <Blog></Blog>,
+      },
+      {
+        path: "/week",
+        element: <WeeklySedule></WeeklySedule>
       },
       {
         path: '/blog-details/:id',
